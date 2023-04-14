14a2d4385982dfea5316e9cf491273e7cab730d0
matteovcc <matteovcc99@gmail.com>
2023-04-14 14:39:48 +0200

    edit

--- src/components/AlbumPage.jsx
+++ src/components/AlbumPage.jsx
@@ -1,1 +1,13 @@
+import MyFooter from "./MyFooter";
+import MyNav from "./MyNav";
 
+const AlbumPage = () => {
+  return (
+    <>
+      <MyNav />
+      <MyFooter />
+    </>
+  );
+};
+
+export default AlbumPage;
---
+++ src/components/ArtistPage.jsx
@@ -0,0 +1,13 @@
+import MyFooter from "./MyFooter";
+import MyNav from "./MyNav";
+
+const ArtistPage = () => {
+  return (
+    <>
+      <MyNav />
+      <MyFooter />
+    </>
+  );
+};
+
+export default ArtistPage;
